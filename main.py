ac4ad00ccdf0d1e0275369eae9b47119550573a0
Jason Sievert <jsievert@gmail.com>
2022-02-03 14:23:00 -0600

    Now showing only instock

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import requests
 import re
+import json
 pattern = "[(.*?)]"
 
-response = requests.get('https://www.microcenter.com/product/621439/raspberry-pi-4-model-b-2gb-ddr4')
-#print(response.text)
-for item in response.text.split("\n"):
-  if "var inventory" in item:
-    result = re.search('\[(.*)\]', item.strip())
-    print(result.group(1))
+search_list = ["https://www.microcenter.com/product/621439/raspberry-pi-4-model-b-2gb-ddr4"]
+
+def get_stock(page):
+  for item in response.text.split("\n"):
+    if "var inventory" in item:
+      #result = re.search('\[(.*)\]', item.strip())
+      result = item.split("= ",1)[1].strip()
+      stock = json.loads(result)
+      for location in stock:
+          if location.get('qoh') > 0: 
+            print("%d at %s" % (location.get('qoh'), location.get('storeName')))
+      #print(json.dumps(stock, sort_keys=True, indent=4))
+      #print(result)
+
+for item in search_list:  
+  response = requests.get(item)
+  page = response.text
+  get_stock(page)
